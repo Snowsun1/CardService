d3e867d8ba022c348b98ba2e3aa4c07f72e4328e
Snowsun1 <lev-seredinskiy@mail.ru>
2021-07-14 02:39:47 +0300

    second

--- src/main/java/com/example/demo/controller/BankCardController.java
+++ src/main/java/com/example/demo/controller/BankCardController.java
@@ -1,10 +1,12 @@
 package com.example.demo.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.service.BankCardService;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @AllArgsConstructor
@@ -13,7 +15,11 @@ public class BankCardController {
 
     @PostMapping("/bank-card/{id}")
     public Long createBankCard(@PathVariable Long id) throws Exception {
-        return bankCardService.addBankCard(id).getId();
+        try {
+            return bankCardService.addBankCard(id).getId();
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "There is no user with this id!", e);
+        }
     }
 
 }
